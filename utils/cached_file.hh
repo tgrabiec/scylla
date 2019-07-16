afcb8ab8ffbd42d9d37d1f0c4c285784b826629c
Tomasz Grabiec <tgrabiec@scylladb.com>
2019-07-16 11:11:20 +0200

    Asd

--- utils/cached_file.hh
+++ utils/cached_file.hh
@@ -30,9 +30,11 @@
 
 using namespace seastar;
 
-class cached_file;
 
-class cached_file {
+// A file access interface with content caching.
+// The underlying file is assumed to not change contents.
+// File altering methods like writing, truncating, etc. are not supported and will throw.
+class cached_file : public file_impl {
     friend class stream;
 public:
     using offset_type = uint64_t;
@@ -41,21 +43,28 @@ public:
     // Must be aligned to _file.disk_read_dma_alignment(). 4K is always safe.
     static const size_t page_size = 4096;
 private:
-    file _file;
-    reader_permit _permit;
-
     struct cached_page {
         temporary_buffer<char> buf;
         cached_page(temporary_buffer<char> buf) : buf(std::move(buf)) {}
     };
-
+    file _file;
+    reader_permit _permit;
     std::map<page_idx_type, cached_page> _cache;
-
-    const offset_type _start;
     const offset_type _size;
-    offset_type _last_page_size; // Ignores _start in case the start lies on the same page.
     page_idx_type _last_page;
 private:
+    void populate_pages(page_idx_type idx, temporary_buffer<char> buf) {
+        while (buf.size() > page_size) {
+            auto page_buf = buf.share();
+            page_buf.trim(page_size);
+            _cache.emplace(idx, cached_page(std::move(page_buf)));
+            buf.trim_front(page_size);
+            ++idx;
+        }
+
+        _cache.emplace(idx, cached_page(std::move(buf)));
+    }
+
     future<temporary_buffer<char>> get_page(page_idx_type idx, const io_priority_class& pc, size_t io_size) {
         auto i = _cache.lower_bound(idx);
         if (i != _cache.end() && i->first == idx) {
@@ -65,14 +74,14 @@ private:
 
         auto io_pages = io_size / page_size;
 
-        page_idx_type leading_pages = 0; // Holds number of pages with index < idx
+        page_idx_type leading_pages = 0; // Holds number of pages to read which have index < idx
         if (i != _cache.begin()) {
             page_idx_type prev_idx = std::prev(i)->first;
             leading_pages = std::min(io_pages / 2, idx - prev_idx - 1);
         }
 
         const auto pages_up_to_area_end = _last_page - idx + 1;
-        page_idx_type trailing_pages = pages_up_to_area_end; // Holds number of pages with index >= idx
+        page_idx_type trailing_pages = pages_up_to_area_end; // Holds number of pages to read with index >= idx
         if (i != _cache.end()) {
             page_idx_type next_idx = std::next(i)->first;
             trailing_pages = std::min(trailing_pages, next_idx - idx);
@@ -81,7 +90,8 @@ private:
         auto read_idx = idx - leading_pages;
         auto buf = temporary_buffer<char>::aligned(_file.memory_dma_alignment(), (leading_pages + trailing_pages) * page_size);
         buf = make_tracked_temporary_buffer(std::move(buf), _permit);
-        return _file.dma_read(read_idx * page_size, buf.get_write(), buf.size(), pc).then([this, idx, leading_pages, read_idx, buf = std::move(buf)] (size_t size) mutable {
+        return _file.dma_read(read_idx * page_size, buf.get_write(), buf.size(), pc).then(
+                [this, leading_pages, read_idx, buf = std::move(buf)] (size_t size) mutable {
             buf.trim(size);
             populate_pages(read_idx, buf.share());
             buf.trim_front(leading_pages * page_size);
@@ -89,10 +99,79 @@ private:
             return std::move(buf);
         });
     }
+
+    void unsupported() {
+        throw_with_backtrace<std::runtime_error>("unsupported operation on a cached_file");
+    }
+public: /* file_impl */
+    future<size_t> read_dma(uint64_t pos, void* buffer, size_t len, const io_priority_class& pc) override {
+        // FIXME
+    }
+
+    future<size_t> read_dma(uint64_t pos, std::vector<iovec> iov, const io_priority_class& pc) override {
+        // FIXME
+    }
+
+    future<temporary_buffer<uint8_t>> dma_read_bulk(uint64_t offset, size_t range_size, const io_priority_class& pc) override {
+        // FIXME
+    }
+
+    future<struct stat> stat() override {
+        return _file.stat();
+    }
+
+    future<uint64_t> size(void) override {
+        return make_ready_future<uint64_t>(_size);
+    }
+
+    future<> close() override {
+        return _file.close();
+    }
+
+    std::unique_ptr<file_handle_impl> dup() {
+        // FIXME: Don't loose caching properties
+        return _file.dup();
+    }
+
+    subscription<directory_entry> list_directory(std::function<future<> (directory_entry de)> next) override {
+        return _file.list_directory(std::move(next));
+    }
+
+    future<size_t> write_dma(uint64_t pos, const void* buffer, size_t len, const io_priority_class& pc) override { unsupported(); }
+    future<size_t> write_dma(uint64_t pos, std::vector<iovec> iov, const io_priority_class& pc) override { unsupported(); }
+    future<> flush() override { unsupported(); }
+    future<> truncate(uint64_t length) override { unsupported(); }
+    future<> discard(uint64_t offset, uint64_t length) override { unsupported(); }
+    future<> allocate(uint64_t position, uint64_t length) override { unsupported(); }
+public:
+    cached_file(file f, offset_type start, offset_type size)
+        : _file(std::move(f))
+        , _size(size)
+    {
+        offset_type last_byte_offset = _size ? (_size - 1) : 0;
+        _last_page = last_byte_offset / page_size;
+    }
+};
+
+
+// impl raw data source impl
+// so that we can create an input stream with it
+//
+
+class file_slice {
+    friend class stream;
+public:
+    using offset_type = uint64_t;
+private:
+    cached_file _file;
+    const offset_type _start;
+    const offset_type _size;
+    offset_type _last_page_size;
+
 public:
     // Generator for subsequent pages of data read from the file.
     class stream {
-        cached_file* _cached_file;
+        file_slice* _f;
         const io_priority_class* _pc;
         page_idx_type _page_idx;
         offset_type _offset_in_page;
@@ -100,8 +179,8 @@ public:
     public:
         stream() = default;
 
-        stream(cached_file& cf, const io_priority_class& pc, page_idx_type start_page, offset_type start_offset_in_page, size_t io_size)
-            : _cached_file(&cf)
+        stream(file_slice& f, const io_priority_class& pc, page_idx_type start_page, offset_type start_offset_in_page, size_t io_size)
+            : _f(&f)
             , _pc(&pc)
             , _page_idx(start_page)
             , _offset_in_page(start_offset_in_page)
@@ -112,13 +191,13 @@ public:
         // Returns empty buffer when end-of-stream is reached.
         // Calls must be serialized.
         future<temporary_buffer<char>> next() {
-            if (_page_idx > _cached_file->_last_page || !_cached_file) {
+            if (_page_idx > _f->_last_page || !_f) {
                 return make_ready_future<temporary_buffer<char>>(temporary_buffer<char>());
             }
-            return _cached_file->get_page(_page_idx, *_pc, _io_size).then([this] (temporary_buffer<char> page) {
+            return _f->get_page(_page_idx, *_pc, _io_size).then([this] (temporary_buffer<char> page) {
                 _io_size = page_size;
-                if (_page_idx == _cached_file->_last_page) {
-                    page.trim(_cached_file->_last_page_size);
+                if (_page_idx == _f->_last_page) {
+                    page.trim(_f->_last_page_size);
                 }
                 page.trim_front(_offset_in_page);
                 _offset_in_page = 0;
@@ -141,7 +220,7 @@ public:
     }
 public:
     // This instance will represent a subset of f consisting of bytes from the range [start, start + size).
-    cached_file(file f, reader_permit permit, offset_type start, offset_type size)
+    file_slice(file f, reader_permit permit, offset_type start, offset_type size)
         : _file(std::move(f))
         , _permit(std::move(permit))
         , _start(start)
@@ -150,26 +229,6 @@ public:
         offset_type last_byte_offset = _start + (_size ? (_size - 1) : 0);
         _last_page_size = (last_byte_offset % page_size) + (_size ? 1 : 0);
         _last_page = last_byte_offset / page_size;
-    }
-
-    // Populates cache from buf assuming that buf contains the data at the front of the area.
-    void populate_front(temporary_buffer<char> buf) {
-        // Align to page start. We can do this because the junk before _start won't be accessed.
-        auto pad = _start % page_size;
-        auto idx = _start / page_size;
-        buf = temporary_buffer<char>(buf.get_write() - pad, buf.size() + pad, buf.release());
-
-        while (buf.size() > page_size) {
-            auto page_buf = buf.share();
-            page_buf.trim(page_size);
-            _cache.emplace(idx, cached_page(std::move(page_buf)));
-            buf.trim_front(page_size);
-            ++idx;
-        }
-
-        if (buf.size() == page_size) {
-            _cache.emplace(idx, cached_page(std::move(buf)));
-        }
     }
 
     // Returns a stream representing the subset of the file starting at pos.
