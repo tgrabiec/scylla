b46cbd7067e279dc4fa525821bdc3ef39dd52f2e
Tomasz Grabiec <tgrabiec@scylladb.com>
2021-02-22 18:55:36 +0100

    cached_file: Make cached pages evictable via LRU

--- test/boost/cached_file_test.cc
+++ test/boost/cached_file_test.cc
@@ -36,6 +36,8 @@
 
 using namespace seastar;
 
+static lru cf_lru;
+
 static sstring read_to_string(cached_file::stream& s, size_t limit = std::numeric_limits<size_t>::max()) {
     sstring b;
     while (auto buf = s.next().get0()) {
@@ -96,7 +98,7 @@ SEASTAR_THREAD_TEST_CASE(test_file_wrapper) {
     auto page_size = cached_file::page_size;
     cached_file::metrics metrics;
     test_file tf = make_test_file(page_size * 3);
-    cached_file cf(tf.f, metrics, 0, page_size * 3);
+    cached_file cf(tf.f, metrics, 0, cf_lru, page_size * 3);
     seastar::file f = make_cached_seastar_file(cf);
 
     BOOST_REQUIRE_EQUAL(tf.contents.substr(0, 1),
@@ -117,7 +119,7 @@ SEASTAR_THREAD_TEST_CASE(test_reading_from_small_file) {
 
     {
         cached_file::metrics metrics;
-        cached_file cf(tf.f, metrics, 0, tf.contents.size());
+        cached_file cf(tf.f, metrics, 0, cf_lru, tf.contents.size());
 
         {
             BOOST_REQUIRE_EQUAL(tf.contents, read_to_string(cf, 0));
@@ -154,11 +156,82 @@ SEASTAR_THREAD_TEST_CASE(test_reading_from_small_file) {
     {
         size_t off = 100;
         cached_file::metrics metrics;
-        cached_file cf(tf.f, metrics, off, tf.contents.size() - off);
+        cached_file cf(tf.f, metrics, off, cf_lru, tf.contents.size() - off);
 
         BOOST_REQUIRE_EQUAL(tf.contents.substr(off), read_to_string(cf, 0));
         BOOST_REQUIRE_EQUAL(tf.contents.substr(off + 2), read_to_string(cf, 2));
         BOOST_REQUIRE_EQUAL(sstring(), read_to_string(cf, 3000));
+    }
+}
+
+SEASTAR_THREAD_TEST_CASE(test_eviction_via_lru) {
+    auto page = cached_file::page_size;
+    test_file tf = make_test_file(page * 3);
+
+    {
+        cached_file::metrics metrics;
+        cached_file cf(tf.f, metrics, 0, cf_lru, tf.contents.size());
+
+        {
+            BOOST_REQUIRE_EQUAL(tf.contents, read_to_string(cf, 0));
+
+            BOOST_REQUIRE_EQUAL(page * 3, metrics.cached_bytes);
+            BOOST_REQUIRE_EQUAL(page * 3, cf.cached_bytes());
+            BOOST_REQUIRE_EQUAL(3, metrics.page_misses);
+            BOOST_REQUIRE_EQUAL(0, metrics.page_evictions);
+            BOOST_REQUIRE_EQUAL(0, metrics.page_hits);
+            BOOST_REQUIRE_EQUAL(3, metrics.page_populations);
+        }
+
+        {
+            cf_lru.evict_all();
+
+            BOOST_REQUIRE_EQUAL(0, metrics.cached_bytes); // change here
+            BOOST_REQUIRE_EQUAL(0, cf.cached_bytes()); // change here
+            BOOST_REQUIRE_EQUAL(3, metrics.page_misses);
+            BOOST_REQUIRE_EQUAL(3, metrics.page_evictions); // change here
+            BOOST_REQUIRE_EQUAL(0, metrics.page_hits);
+            BOOST_REQUIRE_EQUAL(3, metrics.page_populations);
+        }
+
+        {
+            BOOST_REQUIRE_EQUAL(tf.contents, read_to_string(cf, 0));
+
+            BOOST_REQUIRE_EQUAL(page * 3, metrics.cached_bytes); // change here
+            BOOST_REQUIRE_EQUAL(page * 3, cf.cached_bytes());
+            BOOST_REQUIRE_EQUAL(6, metrics.page_misses); // change here
+            BOOST_REQUIRE_EQUAL(3, metrics.page_evictions);
+            BOOST_REQUIRE_EQUAL(0, metrics.page_hits);
+            BOOST_REQUIRE_EQUAL(6, metrics.page_populations); // change here
+        }
+
+        {
+            // Test that the page which is touched is evicted last
+            BOOST_REQUIRE_EQUAL(tf.contents.substr(page, 1), read_to_string(cf, page, 1)); // hit page 1
+
+            BOOST_REQUIRE_EQUAL(6, metrics.page_misses);
+            BOOST_REQUIRE_EQUAL(3, metrics.page_evictions);
+            BOOST_REQUIRE_EQUAL(1, metrics.page_hits);
+            BOOST_REQUIRE_EQUAL(6, metrics.page_populations);
+
+            cf_lru.evict();
+
+            BOOST_REQUIRE_EQUAL(tf.contents.substr(page, 1), read_to_string(cf, page, 1)); // hit page 1
+
+            BOOST_REQUIRE_EQUAL(6, metrics.page_misses);
+            BOOST_REQUIRE_EQUAL(4, metrics.page_evictions); // change
+            BOOST_REQUIRE_EQUAL(2, metrics.page_hits); // change
+            BOOST_REQUIRE_EQUAL(6, metrics.page_populations);
+
+            cf_lru.evict();
+
+            BOOST_REQUIRE_EQUAL(tf.contents.substr(page, 1), read_to_string(cf, page, 1)); // hit page 1
+
+            BOOST_REQUIRE_EQUAL(6, metrics.page_misses);
+            BOOST_REQUIRE_EQUAL(5, metrics.page_evictions); // change
+            BOOST_REQUIRE_EQUAL(3, metrics.page_hits); // change
+            BOOST_REQUIRE_EQUAL(6, metrics.page_populations);
+        }
     }
 }
 
@@ -167,7 +240,7 @@ SEASTAR_THREAD_TEST_CASE(test_invalidation) {
     test_file tf = make_test_file(page_size * 2);
 
     cached_file::metrics metrics;
-    cached_file cf(tf.f, metrics, 0, page_size * 2);
+    cached_file cf(tf.f, metrics, 0, cf_lru, page_size * 2);
 
     // Reads one page, half of the first page and half of the second page.
     auto read = [&] {
@@ -258,7 +331,7 @@ SEASTAR_THREAD_TEST_CASE(test_invalidation_skewed_cached_file) {
 
     size_t offset = page_size / 2;
     cached_file::metrics metrics;
-    cached_file cf(tf.f, metrics, offset, page_size * 2);
+    cached_file cf(tf.f, metrics, offset, cf_lru, page_size * 2);
 
     // Reads one page, half of the first page and half of the second page.
     auto read = [&] {
