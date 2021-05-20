7c60cb1e129bc0437ccdda8f248daa225401fdc9
Tomasz Grabiec <tgrabiec@scylladb.com>
2021-05-20 19:44:55 +0200

    Introduce flat_mutation_reader_v2
    When compacting a mutation fragment stream (e.g. for sstable
    compaction, data query, repair), the compactor needs to accumulate
    range tombstones which are relevant for the yet-to-be-processed range.
    See range_tombstone_accumulator. One problem is that it has unbounded
    memory footprint because the accumulator needs to keep track of all
    the tombstoned ranges which are still active.
    
    Another, although more benign, problem is computational complexity
    needed to maintain that data structure.
    
    The fix is to get rid of the overlap of range tombstones in the
    mutation fragment stream. In v2 of the stream, there is no longer a
    range_tombstone fragment. Deletions of ranges of rows within a given
    partition are represented with range_tombstone_change fragments. At
    any point in the stream there is a single active clustered
    tombstone. It is initially equal to the neutral tombstone when the
    stream of each partition starts. The range_tombstone_change fragment
    type signify changes of the active clustered tombstone. All fragments
    emitted while a given clustered tombstone is active are affected by
    that tombstone. Like with the old range_tombstone fragments, the
    clustered tombstone is independent from the partition tombstone
    carried in partition_start.
    
    The v2 stream is strict about range tombstone trimming. It emits range
    tombstone changes which reflect range tombstones trimmed to query
    restrictions, and fast-forwarding ranges. This makes the stream more
    canonical, meaning that for a given set of writes, querying the
    database should produce the same stream of fragments for a given
    restrictions. There is less ambiguity in how the writes are
    represented in the fragment stream. It wasn't the case with v1. For
    example, A given set of deletions could be produced either as one
    range_tombstone, or may, split and/or deoverlapped with other
    fragments. Making a stream canonical is easier for diff-calculating.
    
    The classes related to mutation fragment streams were cloned:
    flat_mutation_reader_v2, mutation_fragment_v2, and related concepts.
    
    Refs #8625.

--- range_tombstone.cc
+++ range_tombstone.cc
@@ -21,6 +21,7 @@
 
 #include "range_tombstone.hh"
 #include "mutation_fragment.hh"
+#include "mutation_fragment_v2.hh"
 
 std::ostream& operator<<(std::ostream& out, const range_tombstone& rt) {
     if (rt) {
@@ -28,6 +29,10 @@ std::ostream& operator<<(std::ostream& out, const range_tombstone& rt) {
     } else {
         return out << "{range_tombstone: none}";
     }
+}
+
+std::ostream& operator<<(std::ostream& out, const range_tombstone_change& rt) {
+    return out << "{range_tombstone_change: pos=" << rt.position() << ", " << rt.tombstone() << "}";
 }
 
 std::optional<range_tombstone> range_tombstone::apply(const schema& s, range_tombstone&& src)
