f495ef2a20fbc2c6316bd6295467c85825674674
Tomasz Grabiec <tgrabiec@scylladb.com>
2023-03-22 16:49:08 +0100

    db: Exclude keyspace with per-table replication in get_non_local_strategy_keyspaces*()
    This allows update_pending_ranges(), invoked on keyspace creation, to
    succeed. It will update only token-based erms, which is intended
    behavior, since only those need pending ranges updated.
    
    This change will also make node operations like bootstrap, repair,
    etc. to work (not fail) in the presence of keyspaces with per-table
    erms, they will just not be replicated using those algorithms.
    
    Before, these would fail inside get_effective_replication_map(), which
    is forbidden for keyspaces with per-table replication.

--- replica/database.cc
+++ replica/database.cc
@@ -1143,7 +1143,8 @@ std::vector<sstring> database::get_non_local_strategy_keyspaces() const {
     std::vector<sstring> res;
     res.reserve(_keyspaces.size());
     for (auto const& i : _keyspaces) {
-        if (i.second.get_replication_strategy().get_type() != locator::replication_strategy_type::local) {
+        auto&& rs = i.second.get_replication_strategy();
+        if (rs.get_type() != locator::replication_strategy_type::local && !rs.is_per_table()) {
             res.push_back(i.first);
         }
     }
@@ -1154,7 +1155,8 @@ std::unordered_map<sstring, locator::token_effective_replication_map_ptr> databa
     std::unordered_map<sstring, locator::token_effective_replication_map_ptr> res;
     res.reserve(_keyspaces.size());
     for (auto const& i : _keyspaces) {
-        if (i.second.get_replication_strategy().get_type() != locator::replication_strategy_type::local) {
+        auto&& rs = i.second.get_replication_strategy();
+        if (rs.get_type() != locator::replication_strategy_type::local && !rs.is_per_table()) {
             res.emplace(i.first, i.second.get_effective_replication_map());
         }
     }
