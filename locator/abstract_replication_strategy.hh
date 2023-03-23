00a8dde525b272ec238fa599c3f5f86ca4d4f4ba
Tomasz Grabiec <tgrabiec@scylladb.com>
2023-03-23 14:01:49 +0100

    locator: Extract maybe_remove_node_being_replaced()

--- locator/abstract_replication_strategy.hh
+++ locator/abstract_replication_strategy.hh
@@ -455,4 +455,8 @@ private:
     friend class token_effective_replication_map;
 };
 
+void maybe_remove_node_being_replaced(const token_metadata&,
+                                      const abstract_replication_strategy&,
+                                      inet_address_vector_replica_set& natural_endpoints);
+
 }
