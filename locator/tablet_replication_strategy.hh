2dd835b4ff9f6d61c3d6c46c9aba90453205d544
Tomasz Grabiec <tgrabiec@scylladb.com>
2025-01-02 11:27:20 +0100

    tablets: load_balancer: Scale down tablet count to respect per-shard tablet count goal
    The limit is enforced by controlling average per-shard tablet replica
    count in a given DC, which is controlled by per-table tablet
    count. This is effective in respecting the limit on individual shards
    as long as tablet replicas are distributed evenly between shards.
    
    There is no attempt to move tablets around in order to enforce limits
    on individual shards in case of imbalance between shards.
    
    If the average per-shard tablet count exceeds the limit, all tables
    which contribute to it (have replicas in the DC) are scaled down
    by the same factor. Due to rounding up to the nearest power of 2,
    we may overshoot the per-shard goal by at most a factor of 2.
    
    If different DCs want different scale factors of a given table, the
    lowest scale factor is chosen for a given table.
    
    The limit is configurable. It's a global per-cluster config which
    controls how many tablet replicas per shard in total we consider to be
    still ok. It controls tablet allocator behavior, when choosing initial
    tablet count. Even though it's a per-node config, we don't support
    different limits per node. All nodes must have the same value of that
    config. It's similat in that regard to other scheduler config items
    like tablets_initial_scale_factor and target_tablet_size_in_bytes.

--- locator/tablet_replication_strategy.hh
+++ locator/tablet_replication_strategy.hh
@@ -49,6 +49,12 @@ public:
     /// otherwise, cur_tablets is a copy of the current tablet_map.
     /// Runs under group0 guard.
     virtual future<tablet_map> reallocate_tablets(schema_ptr, token_metadata_ptr, tablet_map cur_tablets) const = 0;
+
+    /// Returns desired replication factor in a given DC.
+    /// Note that individual tablets may lag behind desired replication factor in their
+    /// current replica list, as replication factor changes invole table rebuilding transitions
+    /// which are not instantenous.
+    virtual size_t get_replication_factor(const sstring& dc) const = 0;
 };
 
 } // namespace locator
