2d74595e9899ebdec61a53ed7691c89277ff22ff
Tomasz Grabiec <tgrabiec@scylladb.com>
2025-07-07 00:43:50 +0200

    cql3: Fail early on vnode/tablet flavor alter
    Some tests expect this error. Later, prepare_options() will be changed
    in a way which would fail to accept new options in such case before
    vnode/tablet flavor change is detected, tripping the tests.

--- cql3/statements/ks_prop_defs.cc
+++ cql3/statements/ks_prop_defs.cc
@@ -230,6 +230,11 @@ lw_shared_ptr<data_dictionary::keyspace_metadata> ks_prop_defs::as_ks_metadata(s
 lw_shared_ptr<data_dictionary::keyspace_metadata> ks_prop_defs::as_ks_metadata_update(lw_shared_ptr<data_dictionary::keyspace_metadata> old, const locator::token_metadata& tm, const gms::feature_service& feat) {
     locator::replication_strategy_config_options options;
     const auto& old_options = old->strategy_options();
+    auto initial_tablets = get_initial_tablets(old->initial_tablets());
+    auto uses_tablets = initial_tablets.has_value();
+    if (old->uses_tablets() != uses_tablets) {
+        throw exceptions::invalid_request_exception("Cannot alter replication strategy vnode/tablets flavor");
+    }
     auto sc = get_replication_strategy_class();
     if (sc) {
         options = prepare_options(*sc, tm, get_replication_options(), old_options);
