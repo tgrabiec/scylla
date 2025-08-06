a9a41013a16653f94a7c8fab00b446f982797676
Tomasz Grabiec <tgrabiec@scylladb.com>
2025-08-07 00:40:47 +0200

    cql3: ks_prop_defs: Preserve old options
    In 2d9b8f2, semantics of ALTER was changed for tablet-based keyspaces
    which makes "replication" assignment act like +=, where replication
    options are merged with the old options.
    
    This merging is currently performed in the CQL statement level on
    options map, before passing to topology coordinator. This will change
    in later commit, so move merging here. Merging options of flattened
    level will not be correct because it doesn't recognize nested
    collections, like rack lists.
    
    We want:
    
      {'dc1': 3} + {'dc1': ['rack1', 'rack2']} = {'dc1': ['rack1', 'rack2']}
    
    If we merge flattened options, we would get incorrect flattened options:
    
      {'dc1': 3,
       'dc1:0', 'rack1'
       'dc1:1', 'rack2'}
    
    Which cannot be parsed back into ks_prop_defs on the topology coordinator.
    
    Refs https://github.com/scylladb/scylladb/pull/20208#issuecomment-3174728061
    Refs #25549

--- cql3/statements/ks_prop_defs.cc
+++ cql3/statements/ks_prop_defs.cc
@@ -65,6 +65,16 @@ static locator::replication_strategy_config_options prepare_options(
         }
     }
 
+    // #22688 / #20039 - check for illegal, empty options
+    // moved to here. We want to be able to remove dc:s once rf=0,
+    // in which case, the options actually serialized in result mutations
+    // will in extreme cases in fact be empty -> cannot do this check in
+    // verify_options. We only want to apply this constraint on the input
+    // provided by the user
+    if (!rf && options.empty() && !tm.get_topology().get_datacenters().empty()) {
+        throw exceptions::configuration_exception("Configuration for at least one datacenter must be present");
+    }
+
     if (!rack_list_enabled) {
         for (const auto& opt: options) {
             if (std::holds_alternative<locator::rack_list>(opt.second)) {
@@ -101,14 +111,13 @@ static locator::replication_strategy_config_options prepare_options(
         }
     }
 
-    // #22688 / #20039 - check for illegal, empty options (after above expand)
-    // moved to here. We want to be able to remove dc:s once rf=0,
-    // in which case, the options actually serialized in result mutations
-    // will in extreme cases in fact be empty -> cannot do this check in
-    // verify_options. We only want to apply this constraint on the input
-    // provided by the user
-    if (options.empty() && !tm.get_topology().get_datacenters().empty()) {
-        throw exceptions::configuration_exception("Configuration for at least one datacenter must be present");
+    if (uses_tablets) {
+        // We keep previously specified DC factors for safety.
+        for (const auto& opt: old_options) {
+            if (opt.first != ks_prop_defs::REPLICATION_FACTOR_KEY) {
+                options.insert(opt);
+            }
+        }
     }
 
     return options;
